662c769b00f2edcacc5e3a779164b1e0c6029650
codingple <codingple@gmail.com>
2021-03-14 10:53:10 +0900

    OpenDart Calling Issue #2

--- StoreStockData/main.py
+++ StoreStockData/main.py
@@ -16,37 +16,10 @@ dart = OpenDartReader(api_key)
 report_code = '11011'
 fs_div = 'CFS'
 
-def get_debt_ratio(input_df):
-    if not len(input_df):
-        return None
-
-    df = input_df[0]
-
-    fs_df = df.loc[df['sj_div'] == 'BS']
-
-    debtratio = fs_df.loc[
-        (fs_df['account_id'] == 'ifrs-full_Liabilities') | (fs_df['account_id'] == 'ifrs-full_Equity'), ['account_nm',
-                                                                                                         'thstrm_amount',
-                                                                                                         'frmtrm_amount',
-                                                                                                         'bfefrmtrm_amount']]
-
-    debtratio = debtratio.set_index('account_nm')
-    debtratio[['thstrm_amount', 'frmtrm_amount', 'bfefrmtrm_amount']] = debtratio[['thstrm_amount', 'frmtrm_amount',
-                                                                                   'bfefrmtrm_amount']].apply(
-        pd.to_numeric) / 100000000
-    debtratio = debtratio.T
-    debtratio = debtratio.reset_index(drop=False)
-    debtratio.rename(columns={'index': '구분'}, inplace=True)
-    debtratio['구분'] = debtratio['구분'].replace('thstrm_amount', '당기').replace('frmtrm_amount', '전기').replace(
-        'bfefrmtrm_amount', '전전기')
-    debtratio['부채비율'] = debtratio['부채총계'] / debtratio['자본총계'] * 100
-    debtratio['부채비율'] = debtratio['부채비율'].apply(lambda x: round(x, 2))
-    return debtratio
-
-def opendart_finance(stock_code, target_year, report_code, fs_div):
-    #전체 고유번호 구하기
+def get_dart_primary_num():
+    # 전체 고유번호 구하기
     url = 'https://opendart.fss.or.kr/api/corpCode.xml'
-    params = { 'crtfc_key': api_key, }
+    params = {'crtfc_key': api_key, }
 
     r = requests.get(url, params=params)
     try:
@@ -72,6 +45,44 @@ def opendart_finance(stock_code, target_year, report_code, fs_div):
         all_records.append(record)
 
     corp_codes = pd.DataFrame(all_records)
+    return corp_codes
+
+def get_debt_ratio(input_df):
+    if not len(input_df):
+        return None
+
+    df = input_df[0]
+
+    fs_df = df.loc[df['sj_div'] == 'BS']
+
+    try:
+        debtratio = fs_df.loc[
+            (fs_df['account_id'] == 'ifrs-full_Liabilities') |
+            (fs_df['account_id'] == 'ifrs-full_Equity'),
+            ['account_nm',
+             'thstrm_amount',
+             'frmtrm_amount',
+             'bfefrmtrm_amount']]
+
+        debtratio = debtratio.set_index('account_nm')
+        debtratio[['thstrm_amount', 'frmtrm_amount', 'bfefrmtrm_amount']] = debtratio[['thstrm_amount', 'frmtrm_amount',
+                                                                                       'bfefrmtrm_amount']].apply(
+            pd.to_numeric) / 100000000
+        debtratio = debtratio.T
+        debtratio = debtratio.reset_index(drop=False)
+        debtratio.rename(columns={'index': '구분'}, inplace=True)
+        debtratio['구분'] = debtratio['구분'].replace('thstrm_amount', '당기').replace('frmtrm_amount', '전기').replace(
+            'bfefrmtrm_amount', '전전기')
+        debtratio['부채비율'] = debtratio['부채총계'] / debtratio['자본총계'] * 100
+        debtratio['부채비율'] = debtratio['부채비율'].apply(lambda x: round(x, 2))
+        return debtratio
+    except:
+        print("get_debt_ratio failed")
+        return None
+
+
+
+def opendart_finance(corp_codes, stock_code, target_year, report_code, fs_div):
 
     # 종목코드와 매칭되는 고유번호 구하기
     corp_code = -1
@@ -107,18 +118,20 @@ def opendart_finance(stock_code, target_year, report_code, fs_div):
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-  
+
     # 시간 변수
     day = 0
     min = 0
     sec = 0
     year = 0
+    month = 0
     now = datetime.datetime.now()
     date = now.strftime("%Y%m%d")
     prevDay = now.day
     prevMin = now.minute
     prevSec = now.second
     prevYear = now.year
+    prevMonth = now.month
 
     # MongoDB 변수
     client = MongoClient("mongodb://localhost:27017/")
@@ -127,89 +140,99 @@ if __name__ == '__main__':
     collection_sv = db['stockValue']
     collection_fs = db['financialStatement']
 
+    # Dart 변수
+    corp_codes = None
+
     # Loop
-    while (1):
-      now = datetime.datetime.now()
-      date = now.strftime("%Y%m%d")
+    #while (1):
+    now = datetime.datetime.now()
 
-      if (prevDay != day):
-          df = stock.get_market_fundamental_by_ticker(date)
-          df_dict = df.to_dict("index")
-          count = 0
-          for key in dict(df_dict).keys():
-              count += 1
-              print("["+str(count)+"/"+str(len(dict(df_dict)))+"] " + "Updating pykrx/financial_statement")
-              value = df_dict[key]
-              # value_sv = df_sv_dict[key]
+    if prevMonth != month:
+        corp_codes = get_dart_primary_num()
 
-              if (dict(value)['PER'] == 0.0):
-                  roe = 0.0
-              else:
-                  roe = (dict(value)['PBR'] / dict(value)['PER']) * 100
+    if (prevDay != day):
+        df = stock.get_market_fundamental_by_ticker(date)
+        df_dict = df.to_dict("index")
+        count = 0
+        for key in dict(df_dict).keys():
+            count += 1
+            print("["+str(count)+"/"+str(len(dict(df_dict)))+"] " + "Updating pykrx/financial_statement")
+            value = df_dict[key]
+            # value_sv = df_sv_dict[key]
 
-              debt_ratio = None
-              target_year = now.year + 1
-              while debt_ratio is None:
-                  target_year -= 1
-                  df = opendart_finance(key, target_year, report_code, fs_div)
-                  debt_ratio = get_debt_ratio(df)
-                  if target_year < now.year - 5:
-                      break
+            if (dict(value)['PER'] == 0.0):
+                roe = 0.0
+            else:
+                roe = (dict(value)['PBR'] / dict(value)['PER']) * 100
 
-              collection.update_one({"_id": key}, {"$set": {
-                  "name": stock.get_market_ticker_name(key), "BPS": dict(value)['BPS'],
-                  "PER": dict(value)['PER'], "PBR": dict(value)['PBR'],
-                  "EPS": dict(value)['EPS'], "DIV": dict(value)['DIV'],
-                  "DPS": dict(value)['DPS'],
-                  "ROE": roe}}, upsert=True)
+            debt_ratio = None
+            target_year = now.year + 1
+            while debt_ratio is None:
+                target_year -= 1
+                df = opendart_finance(corp_codes, key, target_year, report_code, fs_div)
+                debt_ratio = get_debt_ratio(df)
+                if target_year < now.year - 5:
+                    break
+
+            collection.update_one({"_id": key}, {"$set": {
+                "name": stock.get_market_ticker_name(key), "BPS": dict(value)['BPS'],
+                "PER": dict(value)['PER'], "PBR": dict(value)['PBR'],
+                "EPS": dict(value)['EPS'], "DIV": dict(value)['DIV'],
+                "DPS": dict(value)['DPS'],
+                "ROE": roe}}, upsert=True)
 
 
-              _valid = 0
-              _year = 0
-              _this = 0
-              _last = 0
-              _lastlast = 0
-              if debt_ratio is not None:
-                  _valid = 1
-                  _year = target_year
-                  _this = debt_ratio['부채비율'][0]
-                  _last = debt_ratio['부채비율'][1]
-                  _lastlast = debt_ratio['부채비율'][2]
+            _valid = 0
+            _year = 0
+            _this = 0
+            _last = 0
+            _lastlast = 0
+            if debt_ratio is not None:
+                _valid = 1
+                _year = target_year
+                _this = debt_ratio['부채비율'][0]
+                _last = debt_ratio['부채비율'][1]
+                _lastlast = debt_ratio['부채비율'][2]
 
-              collection_fs.update_one({"_id": key}, {"$set":{
-                  "isValid": _valid,
-                  "year": _year,
-                  "당기_부채비율": _this,
-                  "전기_부채비율": _last,
-                  "전전기_부채비율": _lastlast,
-              }}, upsert=True)
+            collection_fs.update_one({"_id": key}, {"$set":{
+                "isValid": _valid,
+                "year": _year,
+                "당기_부채비율": _this,
+                "전기_부채비율": _last,
+                "전전기_부채비율": _lastlast,
+            }}, upsert=True)
 
-              print("[debug] _valid : "+str(_valid)+
-                    ", _year : "+str(_year)+
-                    ", _this : "+str(_this)+
-                    ", _last : " + str(_last) +
-                    ", _lastlast : " + str(_lastlast)
-                    )
+            print("[debug] _valid : "+str(_valid)+
+                  ", _year : "+str(_year)+
+                  ", _this : "+str(_this)+
+                  ", _last : " + str(_last) +
+                  ", _lastlast : " + str(_lastlast)
+                  )
 
-          prevDay = now.day
-          print("PYKRX INFO / FINANCIAL STATMENT Updated", prevDay, day)
+        prevDay = now.day
+        print("PYKRX INFO / FINANCIAL STATMENT Updated", prevDay, day)
 
-      if (prevSec != sec):
-          df_sv = stock.get_market_ohlcv_by_ticker(date)
-          df_sv_dict = df_sv.to_dict("index")
+    if (prevSec != sec):
+        df_sv = stock.get_market_ohlcv_by_ticker(date)
+        df_sv_dict = df_sv.to_dict("index")
 
-          for key in dict(df_sv_dict).keys():
-              value = df_sv_dict[key]
-              print("[" + str(count) + "/" + str(len(dict(df_dict))) + "] " + "Updating pykrx sv")
+        for key in dict(df_sv_dict).keys():
+            value = df_sv_dict[key]
+            print("[" + str(count) + "/" + str(len(dict(df_dict))) + "] " + "Updating pykrx sv")
 
-              collection_sv.update_one({"_id": key}, {"$set": {
-                  "curPrice": dict(value)['시가'],
-                  "highPrice": dict(value)['고가'], "lowPrice": dict(value)['저가'],
-                  "endPrice": dict(value)['종가'], "volume": dict(value)['거래량'],
-                  "transaction": dict(value)['거래대금'], "fluctuation": dict(value)['등락률']}}, upsert=True)
-          prevSec = now.second
-          print("PYKRX SV Updated", prevMin, min)
+            collection_sv.update_one({"_id": key}, {"$set": {
+                "curPrice": dict(value)['시가'],
+                "highPrice": dict(value)['고가'], "lowPrice": dict(value)['저가'],
+                "endPrice": dict(value)['종가'], "volume": dict(value)['거래량'],
+                "transaction": dict(value)['거래대금'], "fluctuation": dict(value)['등락률']}}, upsert=True)
+        prevSec = now.second
+        print("PYKRX SV Updated", prevMin, min)
 
+    day = now.day
+    min = now.minute
+    sec = now.second
+    year = now.year
+    month = now.month
 
     # cursor = collection.find({})
     # cursor_sv = collection_sv.find({})
